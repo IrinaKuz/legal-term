a3d7e1bf070afb399b324cfbedd66870b6ba1e76
IrinaKuz <ira.kuznets@gmail.com>
2022-01-30 23:24:34 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-    xhttp.open("GET", "terminology.json");
-    xhttp.send();
+fetch('terminology.json')
+  .then(response => response.json())
+  .then(data => {
+    const terminology = data;
+    makeListData(terminology);
+    getAnswerDefinition(terminology);
+  })
+
+function getAnswerDefinition(terminology) {
+    const length = terminology.length;
+    console.log(length);
+    return terminology[Math.random() * length]
+}
+
+function displayDataItem(item){
+  const name = document.createElement('dt');
+  name.innerText = item.name;
+  const defin = document.createElement('dd');
+  defin.innerText = item.def;
+  const p = document.createElement('p');
+  p.append(name, defin);
+  return p;
+}
+
+function makeListData(terminology) {
+  const demo = document.getElementById('demo');
+  const list = document.createElement('dl');
+  for(let i = 0; i < terminology.length; i++) {
+    list.appendChild(displayDataItem(terminology[i]));
   }
-
-// function getAnswerDefenition() {
-//     return terminology[Math.random*terminology.length()]
-// }
-
-// console.log(getAnswerDefenition());
-
-loadDoc();
+  demo.appendChild(list);
+}
